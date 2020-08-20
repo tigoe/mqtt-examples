f56c6d79a5fad19859669d7962c2dacb5f2bc5eb
Tom Igoe <tom.igoe@gmail.com>
2020-08-20 10:24:29 -0400

    Update sketch.js

--- p5js-mqtt-client/sketch.js
+++ p5js-mqtt-client/sketch.js
@@ -7,6 +7,7 @@
     but has also been tested on https://test.mosquitto.org
 
     created 12 June 2020
+    modified 20 Aug 2020
     by Tom Igoe
 */
 
@@ -24,14 +25,18 @@ let creds = {
     password: 'try'
 }
 // topic to subscribe to when you connect:
-let topic = 'time';
+let topic = 'try/notes';
+
+// a pushbutton to send messages
+let sendButton;
+let localDiv;
+let remoteDiv;
+
+// intensity of the circle in the middle
+let intensity = 255;
 
 function setup() {
-    createCanvas(640, 640);
-    // run draw() every two seconds:
-    frameRate(0.5);
-    textSize(32);
-    fill(0);
+    createCanvas(400, 400);
     // Create an MQTT client 
     client = new Paho.MQTT.Client(broker.hostname, Number(broker.port), creds.clientID);
     // set callback handlers for the client:
@@ -45,14 +50,59 @@ function setup() {
             password: creds.password    // password
         }
     );
+    // create the send button:
+    sendButton = createButton('send a message');
+    sendButton.position(20, 20);
+    sendButton.mousePressed(sendMqttMessage);
+    // create a div for local messages:
+    localDiv = createDiv('local messages will go here');
+    localDiv.position(20, 50);
+    localDiv.style('color', '#fff');
+    // create a div for the response:
+    remoteDiv = createDiv('waiting for messages');
+    remoteDiv.position(20, 80);
+    remoteDiv.style('color', '#fff');
 }
 
 function draw() {
-    background(255);
+    background(50);
+    // draw a circle whose brightness changes when a message is received:
+    fill(intensity);
+    circle(width/2, height/2, width/2);
+// subtract one from the brightness of the circle:
+    if (intensity > 0) {
+        intensity--;
+    }
+}
+
+// called when the client connects
+function onConnect() {
+    localDiv.html('client is connected');
+    client.subscribe(topic);
+}
+
+// called when the client loses its connection
+function onConnectionLost(response) {
+    if (response.errorCode !== 0) {
+        localDiv.html('onConnectionLost:' + response.errorMessage);
+    }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+    remoteDiv.html('I got a message:' + message.payloadString);
+    let  incomingNumber = parseInt(message.payloadString);
+    if (incomingNumber > 0) {
+        intensity = 255;
+    }
+}
+
+// called when you want to send a message:
+function sendMqttMessage() {
     // if the client is connected to the MQTT broker:
     if (client.isConnected()) {
-        // make a string with the current time:
-        let msg = 'The time: ' + hour() + ':' + minute() + ':' + second();
+        // make a string with a random number form 0 to 15:
+        let msg = String(round(random(15)));
         // start an MQTT message:
         message = new Paho.MQTT.Message(msg);
         // choose the destination topic:
@@ -60,25 +110,6 @@ function draw() {
         // send it:
         client.send(message);
         // print what you sent:
-        text('I sent: ' + message.payloadString, 10, 50);
+        localDiv.html('I sent: ' + message.payloadString);
     }
-}
-
-// called when the client connects
-function onConnect() {
-    console.log('client is connected');
-    client.subscribe(topic);
-}
-
-// called when the client loses its connection
-function onConnectionLost(response) {
-    if (response.errorCode !== 0) {
-        console.log('onConnectionLost:' + response.errorMessage);
-    }
-}
-
-// called when a message arrives
-function onMessageArrived(message) {
-    text('I got a message:' + message.payloadString, 10, 100);
-    console.log('I got a message: ' + message.payloadString);
 }
