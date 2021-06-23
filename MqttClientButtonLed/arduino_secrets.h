2e7bce5e67cb9da02b16d020587b9173eacef0c9
Tom Igoe <tom.igoe@gmail.com>
2021-06-23 10:18:19 -0400

    changes

--- MqttClientButtonLed/arduino_secrets.h
+++ MqttClientButtonLed/arduino_secrets.h
@@ -1,4 +1,4 @@
-#define SECRET_SSID "sandbox370"
-#define SECRET_PASS "+s0a+s03!2gether?"
+#define SECRET_SSID ""
+#define SECRET_PASS ""
 #define SECRET_MQTT_USER "public"
 #define SECRET_MQTT_PASS "public"
