9d754cf4aaefab07bd6ddbe7ada2808fe87b2246
Tom Igoe <tom.igoe@gmail.com>
2023-01-04 18:26:04 -0500

    Updated MqttLightSender

--- arduino-clients/MqttLightSender/MqttLightSender.ino
+++ arduino-clients/MqttLightSender/MqttLightSender.ino
@@ -1,105 +1,175 @@
 /*
-  Datalogger MQTT client
+  MQTT Client  Light sensor sender/receiver
 
-  Connects to an MQTT broker and uploads data.
+  This sketch demonstrates an MQTT client that connects to a broker, subscribes to a topic,
+  and both listens for messages on that topic and sends messages to it, a random number between 0 and 255.
+  When the client receives a message, it parses it, and PWMs the built-in LED.
 
-  Works with MKR1010, MKR1000, Nano 33 IoT
-  Uses the following libraries:
-   http://librarymanager/All#WiFi101  // use this for MKR1000
-   http://librarymanager/All#WiFiNINA  // use this for MKR1010, Nano 33 IoT
-   http://librarymanager/All#ArduinoMqttClient
-   http://librarymanager/All#Adafruit_TCS34725 (for the sensor)
+  Uses a TCS34725 light sensor to read lux and color temperature
 
-  created 13 Jun 2021
-  modified 14 Nov 2022
+  This sketch uses https://public.cloud.shiftr.io as the MQTT broker, but others will work as well.
+  See https://tigoe.github.io/mqtt-examples/#broker-client-settings for connection details. 
+
+Libraries used:
+  * http://librarymanager/All#WiFiNINA or
+  * http://librarymanager/All#WiFi101 
+  * http://librarymanager/All#ArduinoMqttClient
+  * http://librarymanager/All#Adafruit_TCS34725 (for the sensor)
+
+  the arduino_secrets.h file:
+  #define SECRET_SSID ""    // network name
+  #define SECRET_PASS ""    // network password
+  #define SECRET_MQTT_USER "public" // broker username
+  #define SECRET_MQTT_PASS "public" // broker password
+
+  created 11 June 2020
+  updated 4 Jan 2023
   by Tom Igoe
 */
-// include required libraries and config files
-//#include <WiFi101.h>        // for MKR1000 modules
-#include <WiFiNINA.h>  // for MKR1010 modules and Nano 33 IoT modules
+
+#include <WiFiNINA.h>  // use this for Nano 33 IoT, MKR1010, Uno WiFi
+// #include <WiFi101.h>    // use this for MKR1000
 #include <ArduinoMqttClient.h>
-// I2C and light sensor libraries:
 #include <Wire.h>
 #include <Adafruit_TCS34725.h>
-// network names and passwords:
 #include "arduino_secrets.h"
 
-// initialize WiFi connection using SSL
-// (use WIFiClient and port number 1883 for unencrypted connections):
-WiFiClient wifi;
+// initialize WiFi connection as SSL:
+WiFiSSLClient wifi;
 MqttClient mqttClient(wifi);
-String addressString = "";
+
 
 // details for MQTT client:
-char broker[] = "test.mosquitto.org";
-int port = 1883;
-char topic[] = "undnet/tigoe";
-String clientID = "light-client-tigoe";
+char broker[] = "public.cloud.shiftr.io";
+int port = 8883;
+char topic[] = "conndev/light";
+String clientID = "light-client";
 
-// timestamp for the sensor reading and send:
-long lastSendTime = 0;
+// last time the client sent a message, in ms:
+long lastTimeSent = 0;
+// message sending interval:
+int interval = 60 * 1000;
 
-// interval between requests:
-int sendInterval = 60 * 1000;
 // initialize the light sensor:
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_600MS, TCS34725_GAIN_1X);
 
+String macAddr;
+
 void setup() {
-  Serial.begin(9600);  // initialize serial communication
-  // if serial monitor is not open, wait 3 seconds:
+  // initialize serial:
+  Serial.begin(9600);
+  // wait for serial monitor to open:
   if (!Serial) delay(3000);
+  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize WiFi, if not connected:
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Connecting to ");
+    Serial.println(SECRET_SSID);
+    WiFi.begin(SECRET_SSID, SECRET_PASS);
+    delay(2000);
+  }
+  // get MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  for (int i = 5; i >= 0; i--) {
+    if (mac[i] < 16) macAddr += "0";
+    macAddr += String(mac[i], HEX);
+  }
+
+  Serial.println();
+  // print IP address once connected:
+  Serial.print("Connected. My IP address: ");
+  Serial.println(WiFi.localIP());
 
   // set the credentials for the MQTT client:
-   mqttClient.setId(clientID);
-  // // login to the broker with a username and password:
-  // mqttClient.setUsernamePassword(SECRET_MQTT_USER, SECRET_MQTT_PASS);
-
-// connect to WiFi
-  connectToNetwork();
-
-  // try to connect to the MQTT broker once you're connected to WiFi:
-  while (!connectToBroker()) {
-    Serial.println("attempting to connect to broker");
-    delay(1000);
-  }
-  Serial.println("connected to broker");
+  mqttClient.setId(clientID);
+  // if needed, login to the broker with a username and password:
+  mqttClient.setUsernamePassword(SECRET_MQTT_USER, SECRET_MQTT_PASS);
 }
 
 void loop() {
   //if you disconnected from the network, reconnect:
   if (WiFi.status() != WL_CONNECTED) {
     connectToNetwork();
+    // skip the rest of the loop until you are connected:
+    return;
   }
 
   // if not connected to the broker, try to connect:
   if (!mqttClient.connected()) {
-    Serial.println("reconnecting to broker");
+    Serial.println("attempting to connect to broker");
     connectToBroker();
-  } else {
-    // If the client is not connected:  if (!client.connected()) {
-    // and the request interval has passed:
-    if (millis() - lastSendTime >= sendInterval) {
-      // read the sensor
-      // get lux and color temperature from sensor:
-      uint16_t r, g, b, c, colorTemp, lux;
-      tcs.getRawData(&r, &g, &b, &c);
-      colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
-      lux = tcs.calculateLux(r, g, b);
-      String body = "{\"lux\": ll, \"ct\": cc}";
-      body.replace("ll", String(lux));
-      body.replace("cc", String(colorTemp));
+  }
+  // poll for new messages from the broker:
+  mqttClient.poll();
 
-      // start the mesage:
+  // once every interval, send a message:
+  if (millis() - lastTimeSent > interval) {
+    // read the sensor
+    // get lux and color temperature from sensor:
+    uint16_t r, g, b, c, colorTemp, lux;
+    tcs.getRawData(&r, &g, &b, &c);
+    colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
+    lux = tcs.calculateLux(r, g, b);
+    String message = "{\"lux\": LUX, \"ct\": CT, \"id\":ID}";
+    message.replace("LUX", String(lux));
+    message.replace("CT", String(colorTemp));
+    message.replace("CT", String(colorTemp));
+    message.replace("ID", macAddr);
+
+    if (mqttClient.connected()) {
+      // start a new message on the topic:
       mqttClient.beginMessage(topic);
-      // add the value:
-      mqttClient.println(body);
+      // print the body of the message:
+      mqttClient.print(message);
       // send the message:
       mqttClient.endMessage();
-
-      // take note of the time you make your request:
-      lastSendTime = millis();
+      // send a serial notification:
+      Serial.print("published a message: ");
+      Serial.println(message);
+      // timestamp this message:
+      lastTimeSent = millis();
     }
   }
+}
+
+boolean connectToBroker() {
+  // if the MQTT client is not connected:
+  if (!mqttClient.connect(broker, port)) {
+    // print out the error message:
+    Serial.print("MOTT connection failed. Error no: ");
+    Serial.println(mqttClient.connectError());
+    // return that you're not connected:
+    return false;
+  }
+
+  // set the message receive callback:
+  mqttClient.onMessage(onMqttMessage);
+  // subscribe to a topic:
+  Serial.print("Subscribing to topic: ");
+  Serial.println(topic);
+  mqttClient.subscribe(topic);
+
+  // once you're connected, you
+  // return that you're connected:
+  return true;
+}
+
+void onMqttMessage(int messageSize) {
+  // we received a message, print out the topic and contents
+  Serial.println("Received a message with topic ");
+  Serial.print(mqttClient.messageTopic());
+  Serial.print(", length ");
+  Serial.print(messageSize);
+  Serial.println(" bytes:");
+  String incoming = "";
+  // use the Stream interface to print the contents
+  while (mqttClient.available()) {
+    incoming += (char)mqttClient.read();
+  }
+
+  // print the incoming message:
+  Serial.println(incoming);
 }
 
 
@@ -112,19 +182,4 @@ void connectToNetwork() {
     delay(2000);
   }
   Serial.println("connected.");
-}
-
-boolean connectToBroker() {
-  // if the MQTT client is not connected:
-  if (!mqttClient.connect(broker, port)) {
-    // print out the error message:
-    Serial.print("MOTT connection failed. Error no: ");
-    Serial.println(mqttClient.connectError());
-    // return that you're not connected:
-    return false;
-  }
-  mqttClient.subscribe(topic);
-  // once you're connected, you
-  // return that you're connected:
-  return true;
 }
