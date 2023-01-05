8ec9d08e07a51fcbd3ba81c8477f73e92c0148d3
Tom Igoe <tom.igoe@gmail.com>
2023-01-05 07:04:56 -0500

    Updated NeoPixel example, deleted outdated examples

--- arduino-clients/MqttClientNeoPixel/MqttClientNeoPixel.ino
+++ arduino-clients/MqttClientNeoPixel/MqttClientNeoPixel.ino
@@ -1,18 +1,22 @@
 /*
-  MQTT Client with a button and an LED
+  MQTT NeoPixel Client 
 
-  This sketch demonstrates an MQTT client that connects to a broker, subsrcibes to a topic,
-  and both listens for messages on that topic and sends messages to it. When a pushbutton
-  is pressed, the client sends a message, a random number between 0 and 15.
-  When the client receives a message, it parses it, and if the number is greater than 0,
-  it sets an LED to full. When nothing is happening,
-  if the LED is not off, it's faded down one point every time through the loop.
+  This sketch shows how to control NeoPixels with MQTT. 
+  It expects a comma-separated string of values for red, green, and blue
+  in the incoming topic. When it gets that, it uses the values to 
+  change the color of the strip. 
 
-  This sketch uses https://next.shiftr.io/try as the MQTT broker.
+  This sketch uses https://public.cloud.shiftr.io as the MQTT broker, but others will work as well.
+  See https://tigoe.github.io/mqtt-examples/#broker-client-settings for connection details. 
 
-  the circuit:
-  - pushbutton attached to pin 2, connected to ground
-  - LED's anode connected to pin 3, cathode connected to ground.
+  The circuit:
+  * NeoPixels attached to pin 5
+
+Libraries used:
+  * http://librarymanager/All#WiFiNINA or
+  * http://librarymanager/All#WiFi101 
+  * http://librarymanager/All#ArduinoMqttClient
+  * http://librarymanager/All#Adafruit_NeoPixel
 
   the arduino_secrets.h file:
   #define SECRET_SSID ""    // network name
@@ -21,101 +25,67 @@
   #define SECRET_MQTT_PASS "public" // broker password
 
   created 11 June 2020
+  updated 5 Jan 2023
   by Tom Igoe
 */
 
-#include <WiFiNINA.h>
+
+#include <WiFiNINA.h>  // use this for Nano 33 IoT, MKR1010, Uno WiFi
+// #include <WiFi101.h>    // use this for MKR1000
 #include <ArduinoMqttClient.h>
-#include "arduino_secrets.h"
 #include <Adafruit_NeoPixel.h>
-const int neoPixelPin = 5;  // control pin
-const int pixelCount = 7;    // number of pixels
+#include "arduino_secrets.h"
 
-// set up strip:
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(pixelCount, neoPixelPin, NEO_GRBW + NEO_KHZ800);
-
-
-// initialize WiFi connection:
-WiFiClient wifi;
+// initialize WiFi connection as SSL:
+WiFiSSLClient wifi;
 MqttClient mqttClient(wifi);
+
+const int neoPixelPin = 5;  // control pin
+const int pixelCount = 7;   // number of pixels
+
+// set up NeoPixels:
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(pixelCount, neoPixelPin, NEO_GRBW + NEO_KHZ800);
 
 // details for MQTT client:
 char broker[] = "public.cloud.shiftr.io";
-int port = 1883;
+int port = 8883;
 char topic[] = "color";
 char clientID[] = "neoPixelClient";
 
 // intensity of LED:
 int intensity = 0;
 
-// details for pushbutton and LED:
-const int buttonPin = 2;
-const int ledPin = 3;
-const int debounceDelay = 5;
-int lastButtonState = 0;
-
 void setup() {
   // initialize serial:
   Serial.begin(9600);
   // wait for serial monitor to open:
-  while (!Serial);
-
-  // initialize I/O pins:
-  pinMode(buttonPin, INPUT_PULLUP);
-  pinMode (ledPin, OUTPUT);
-
-  // initialize WiFi, if not connected:
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Connecting to ");
-    Serial.println(SECRET_SSID);
-    WiFi.begin(SECRET_SSID, SECRET_PASS);
-    delay(2000);
-  }
-  // print IP address once connected:
-  Serial.print("Connected. My IP address: ");
-  Serial.println(WiFi.localIP());
+  if (!Serial) delay(3000);
+  // connect to WiFi:
+  connectToNetwork();
 
   // set the credentials for the MQTT client:
   mqttClient.setId(clientID);
+  // if needed, login to the broker with a username and password:
   mqttClient.setUsernamePassword(SECRET_MQTT_USER, SECRET_MQTT_PASS);
-
-  // try to connect to the MQTT broker once you're connected to WiFi:
-  while (!connectToBroker()) {
-    Serial.println("attempting to connect to broker");
-    delay(1000);
-  }
-  Serial.println("connected to broker");
+  // initialize the NeoPixels:
   strip.begin();
 }
 
 void loop() {
+  //if you disconnected from the network, reconnect:
+  if (WiFi.status() != WL_CONNECTED) {
+    connectToNetwork();
+    // skip the rest of the loop until you are connected:
+    return;
+  }
+
   // if not connected to the broker, try to connect:
   if (!mqttClient.connected()) {
-    Serial.println("reconnecting");
+    Serial.println("attempting to connect to broker");
     connectToBroker();
   }
-
-  // if a message comes in, read it:
-  if (mqttClient.parseMessage() > 0) {
-    Serial.print("Got a message on topic: ");
-    Serial.println(mqttClient.messageTopic());
-    // read the message:
-    while (mqttClient.available()) {
-      int r = mqttClient.parseInt();
-      int g = mqttClient.parseInt();
-      int b = mqttClient.parseInt();
-      int w = mqttClient.parseInt();
-      Serial.print(r);
-      Serial.print(",");
-      Serial.print(g);
-      Serial.print(",");
-      Serial.println(b);
-
-      unsigned long color = (w << 24) + (r << 16) + (g << 8) + b;
-      strip.fill(color);
-    }
-  }
-  strip.show();
+  // poll for new messages from the broker:
+  mqttClient.poll();
 }
 
 boolean connectToBroker() {
@@ -127,8 +97,52 @@ boolean connectToBroker() {
     // return that you're not connected:
     return false;
   }
-  // once you're connected, you can proceed:
+
+  // set the message receive callback:
+  mqttClient.onMessage(onMqttMessage);
+  // subscribe to a topic and all subtopics:
+  Serial.print("Subscribing to topic: ");
+  Serial.println(topic);
   mqttClient.subscribe(topic);
+
+  // once you're connected, you
   // return that you're connected:
   return true;
+}
+
+void onMqttMessage(int messageSize) {
+  // initialize variables for red, green, blue:
+  int r, g, b = 0;
+  // we received a message, print out the topic and contents
+  Serial.println("Received a message with topic ");
+  Serial.println(mqttClient.messageTopic());
+Serial.print(" values: ");
+  // read the incoming message as a comma-separated string of values:
+  while (mqttClient.available()) {
+    int r = mqttClient.parseInt();
+    int g = mqttClient.parseInt();
+    int b = mqttClient.parseInt();
+    Serial.print(r);
+    Serial.print(",");
+    Serial.print(g);
+    Serial.print(",");
+    Serial.println(b);
+    // fill the strip with color:
+    strip.fill(r, g, b);
+    // update the strip:
+    strip.show();
+  }
+}
+
+void connectToNetwork() {
+  // try to connect to the network:
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Attempting to connect to: " + String(SECRET_SSID));
+    //Connect to WPA / WPA2 network:
+    WiFi.begin(SECRET_SSID, SECRET_PASS);
+    delay(2000);
+  }
+  // print IP address once connected:
+  Serial.print("Connected. My IP address: ");
+  Serial.println(WiFi.localIP());
 }
--- arduino-clients/readme.md
+++ arduino-clients/readme.md
@@ -1,11 +1,13 @@
 ## Arduino MQTT Clients
 The [ArduinoMqttClient](https://github.com/arduino-libraries/ArduinoMqttClient) library makes it easy to send and receive MQTT messages using WiFi-enabled Arduino models such as the Nano 33 IoT, MKR1010, MKR1000, or other third-party devices with compatible WiFi libraries. This repository contains examples using this library. All of these will work with the basic MQTT client examples in the [JavaScript Clients]({{site.baseurl}}/#javascript-clients) section of the repository, if you match up the public brokers they are all using.
 
-## ArduinoMqttClient
 
-[See the source code]({{site.codeurl}}/arduino-clients/ArduinoMqttClient/ArduinoMqttClient.ino) 
+### Installing the library and Board Definitions
 
-This is a basic  client example for the ArduinoMqttClient library. The global variables define the broker and credentials. In the setup, the Arduino connects to WiFi and sets the MQTT client ID and other pre-connect characteristics, including the `onMessage` handler. In the loop, it continually attempts to connect to WiFi if it's not connected using the custom `connectToNetwork()` function; attempts to connect to the broker if not connected using the custom `connectToBroker()` function; then polls for any incoming MQTT messages using the `onMqttMessage()` handler. Then, once every interval, it reads a sensor and publishes the value as an MQTT message on the topic.
+If you've never used one of the WiFi-capable Arduino boards, you'll need to install the board definition. When you plug the board in, the Arduino IDE should pop up a message asking if you want to install the board definition. If not, click the Tools Menu --> Boards... --> Board Manager, and search for the board you're using. When you find it, click Install. The Nano 33 IoT, the MKR 1010 and the Uno WiFi all use the WiFiNINA library, and the MKR1000 uses the WiFi101 library. These two libraries have identical APIs.
+
+To install the [ArduinoMqttClient](https://www.arduino.cc/reference/en/libraries/arduinomqttclient/) library, click the Sketch Menu --> Include Library --> Manage Libraries, and search for ArduinoMqttClient. When you find it, click Install. Then do the same for the [WiFiNINA](https://www.arduino.cc/reference/en/libraries/wifinina/) library (if you're using a Nano 33 IoT or MKR 1010) or the [WiFi101](https://www.arduino.cc/reference/en/libraries/wifi101/) library (if you're using a MKR1000).
+
 
 All of the Arduino examples in this repository require an extra file to be included to the sketch. Use command-shift-N on MacOS or or control-shift-N on Windows, then name the file `arduino_secrets.h` The following credentials should be included in that file:
 
@@ -15,6 +17,15 @@ All of the Arduino examples in this repository require an extra file to be inclu
 #define SECRET_MQTT_USER "public" // broker username for shiftr.io
 #define SECRET_MQTT_PASS "public" // broker password for shiftr.io
 ````
+
+Note: this assumes you're on a network with WPA2 encryption, which is what most people are using at home these days. If you're not, check the [WiFiNINA library documentation](https://www.arduino.cc/reference/en/libraries/wifinina/) for how to configure your sketches for other network types. 
+
+
+## ArduinoMqttClient
+
+[See the source code]({{site.codeurl}}/arduino-clients/ArduinoMqttClient/ArduinoMqttClient.ino) 
+
+This is a basic  client example for the ArduinoMqttClient library. The global variables define the broker and credentials. In the setup, the Arduino connects to WiFi and sets the MQTT client ID and other pre-connect characteristics, including the `onMessage` handler. In the loop, it continually attempts to connect to WiFi if it's not connected using the custom `connectToNetwork()` function; attempts to connect to the broker if not connected using the custom `connectToBroker()` function; then polls for any incoming MQTT messages using the `onMqttMessage()` handler. Then, once every interval, it reads a sensor and publishes the value as an MQTT message on the topic.
 
 The other Arduino examples in this repository follow more or less the same structure. 
 
@@ -42,3 +53,10 @@ This example reads temperature and humidity levels from an  [Sensiron SHTC3 Temp
 [See the source code]({{site.codeurl}}/arduino-clients/MqttClientSubTopics/MqttClientSubTopics.ino)
 
 This example shows how to use MQTT subtopics to get and set the properties of a microcontroller's program. The microcontroller has two blinking LEDs and an analog sensor attached. You can change the behavior of the LEDs remotely by publishing to the subtopics `/brightness` and `/blinkInterval` and change the sensor update rate by publishing to `/sendInterval`.
+
+
+## MqttClientNeoPixel
+
+[See the source code]({{site.codeurl}}/arduino-clients/MqttClientNeoPixel/MqttClientNeoPixel.ino)
+
+This example shows how to use MQTT subtopics to set the color of a strip of WorldSemi [WS2812 addressable LEDs (NeoPixels)](https://tigoe.github.io/LightProjects/addressable-leds). The microcontroller subscribes to a topic called `color` and looks for a comma-separated string of values, r, g, and b. When it gets these, it uses them to update the NeoPixel's colors.
